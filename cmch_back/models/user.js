0080801c9bb5f7fc385367ea83639eba5d151e9e
Nabil-MEDIOUNI <nabilmediouni2000@gmail.com>
2020-04-18 15:41:54 +0300

    done

--- cmch_back/models/user.js
+++ cmch_back/models/user.js
@@ -34,6 +34,10 @@ const userSchema = new mongoose.Schema(
             type: Array,
             default: []
         },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
         seller: {
             type: ObjectId,
             ref: "Seller",
