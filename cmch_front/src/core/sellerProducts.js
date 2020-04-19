1613fa8a8fffa718d95496119302d5830e7ea00e
unknown <ahmedjbeli54@yahoo.fr>
2020-04-19 05:50:20 +0100

    seller products

--- cmch_front/src/core/sellerProducts.js
+++ cmch_front/src/core/sellerProducts.js
@@ -5,13 +5,13 @@ import { getSellerProduct} from "./apiCore";
 
 
 
-const ShowProducts = (props) =>{
+const ShowProducts = props =>{
     const [product, setProduct] = useState(0)
+   
     useEffect(()=>{
-        const sellerId = this.props.match.params
-        console.log(sellerId)
+        const sellerId = props.match.params.sellerId
         getSellerProduct(sellerId).then(
-            data => {if(!data.error) setProduct(data)}
+            data => {setProduct(data)}
         )
     },[])
 
@@ -20,11 +20,12 @@ const ShowProducts = (props) =>{
              <div className="col-8">
                     <h2 className="mb-4">Products</h2>
                     <div className="row">
-                        {product.map((product, i) => (
+                        {console.log(product)}
+                        {/* {product.map((product, i) => (
                             <div key={i} className="col-4 mb-3">
                                 <Card product={product} />
                             </div>
-                        ))}
+                        ))} */}
                     </div>
                     <hr />
                     {/* {loadMoreButton()} */}
