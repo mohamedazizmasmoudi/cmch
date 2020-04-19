8dc928890a811cb1e0887abe39401ff0d79af297
Nabil-MEDIOUNI <nabilmediouni2000@gmail.com>
2020-04-19 13:18:10 +0300

    zedet fitlter

--- cmch_front/src/core/Checkbox.js
+++ cmch_front/src/core/Checkbox.js
@@ -1,35 +1,40 @@
 import React, { useState } from "react";
 
-const Checkbox = ({ categories, handleFilters }) => {
-    const [checked, setCheked] = useState([]);
+const Checkbox = ({ categories, handleFilters, pictures }) => {
+  const [checked, setCheked] = useState([]);
 
-    const handleToggle = c => () => {
-        // return the first index or -1
-        const currentCategoryId = checked.indexOf(c);
-        const newCheckedCategoryId = [...checked];
-        // if currently checked was not already in checked state > push
-        // else pull/take off
-        if (currentCategoryId === -1) {
-            newCheckedCategoryId.push(c);
-        } else {
-            newCheckedCategoryId.splice(currentCategoryId, 1);
-        }
-        // console.log(newCheckedCategoryId);
-        setCheked(newCheckedCategoryId);
-        handleFilters(newCheckedCategoryId);
-    };
+  const handleToggle = (c) => () => {
+    // return the first index or -1
+    const currentCategoryId = checked.indexOf(c);
+    const newCheckedCategoryId = [...checked];
+    // if currently checked was not already in checked state > push
+    // else pull/take off
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(c);
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    // console.log(newCheckedCategoryId);
+    setCheked(newCheckedCategoryId);
+    handleFilters(newCheckedCategoryId);
+  };
 
-    return categories.map((c, i) => (
-        <li key={i} className="list-unstyled">
-            <input
-                onChange={handleToggle(c._id)}
-                value={checked.indexOf(c._id === -1)}
-                type="checkbox"
-                className="form-check-input"
-            />
-            <label className="form-check-label">{c.name}</label>
-        </li>
-    ));
+  return categories.map((c, i) => (
+    <li key={i} className="list-unstyled">
+          <img
+            key={i}
+            src={pictures}
+            value={checked.indexOf(c._id === -1)}
+            onClick={handleToggle(c._id)}
+          />
+          <label
+            style={{ fontSize: 20, paddingBottom: 50 }}
+            className="form-check-label"
+          >
+            {c.name}
+          </label>
+    </li>
+  ));
 };
 
 export default Checkbox;
--- cmch_front/src/core/Shop.js
+++ cmch_front/src/core/Shop.js
@@ -3,120 +3,128 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories, getFilteredProducts } from "./apiCore";
 import Checkbox from "./Checkbox";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Footer from "./Footer";
 import Menu from "./Menu";
-
-
+import img1 from "../utils/img/khadhar.jpg";
+import img2 from "../utils/img/_3atar.jpg";
+import img3 from "../utils/img/tawabel.jpg";
 const Shop = () => {
-    const [myFilters, setMyFilters] = useState({
-        filters: { category: []}
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [] },
+  });
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
+  const [filteredResults, setFilteredResults] = useState([]);
+
+  const init = () => {
+    getCategories().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setCategories(data);
+      }
     });
-    const [categories, setCategories] = useState([]);
-    const [error, setError] = useState(false);
-    const [limit, setLimit] = useState(6);
-    const [skip, setSkip] = useState(0);
-    const [size, setSize] = useState(0);
-    const [filteredResults, setFilteredResults] = useState([]);
+  };
 
-    const init = () => {
-        getCategories().then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setCategories(data);
-            }
-        });
-    };
+  const loadFilteredResults = (newFilters) => {
+    // console.log(newFilters);
+    getFilteredProducts(skip, limit, newFilters).then((data) => {
+      console.log(data);
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults(data.sellercategoryy);
+        setSize(data.size);
+        setSkip(0);
+      }
+    });
+  };
 
-    const loadFilteredResults = newFilters => {
-        // console.log(newFilters);
-        getFilteredProducts(skip, limit, newFilters).then(data => {
-            console.log(data)
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setFilteredResults(data.sellercategoryy);
-                setSize(data.size);
-                setSkip(0);
-            }
-        });
-    };
+  const loadMore = () => {
+    let toSkip = skip + limit;
+    // console.log(newFilters);
+    getFilteredProducts(toSkip, limit, myFilters.filters).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.sellercategoryy]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
 
-    const loadMore = () => {
-        let toSkip = skip + limit;
-        // console.log(newFilters);
-        getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
-            if (data.error) {
-                setError(data.error);
-            } else {
-                setFilteredResults([...filteredResults, ...data.sellercategoryy]);
-                setSize(data.size);
-                setSkip(toSkip);
-            }
-        });
-    };
-
-    const loadMoreButton = () => {
-        return (
-            size > 0 &&
-            size >= limit && (
-                <button onClick={loadMore} className="btn btn-warning mb-5">
-                    Load more
-                </button>
-            )
-        );
-    };
-
-    useEffect(() => {
-        init();
-        loadFilteredResults(skip, limit, myFilters.filters);
-    }, []);
-
-    const handleFilters = (filters, filterBy) => {
-        // console.log("SHOP", filters, filterBy);
-        const newFilters = { ...myFilters };
-        newFilters.filters[filterBy] = filters;
-        loadFilteredResults(myFilters.filters);
-        setMyFilters(newFilters);
-    };
-
-
+  const loadMoreButton = () => {
     return (
-        <>
-        <Menu />
-            <div className="row">
-                <div className="col-4">
-                    <h4>Filter by categories</h4>
-                    <ul>
-                        <Checkbox
-                            categories={categories}
-                            handleFilters={filters =>
-                                handleFilters(filters, "category")
-                            }
-                        />
-                    </ul>
-                </div>
-
-                <div className="col-8">
-                    <h2 className="mb-4">Sellers</h2>
-                    <div className="row">
-                        {filteredResults.map((seller, i) => (
-                            <div key={i} className="col-4 mb-3">
-                                <div className="card ">
-                                    <div className="card-header card-header-1 ">{seller.name}</div>
-                                    <Link to={`/shop/seller/${seller._id}`}>check seller</Link>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                    <hr />
-                    {loadMoreButton()}
-                </div>
-            </div>
-            <Footer position/>
-        </>
+      size > 0 &&
+      size >= limit && (
+        <button onClick={loadMore} className="btn btn-warning mb-5">
+          Load more
+        </button>
+      )
     );
+  };
+
+  useEffect(() => {
+    init();
+    loadFilteredResults(skip, limit, myFilters.filters);
+  }, []);
+
+  const handleFilters = (filters, filterBy) => {
+    // console.log("SHOP", filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    loadFilteredResults(myFilters.filters);
+    setMyFilters(newFilters);
+  };
+  const photos = [img1, img2, img3];
+  console.log(photos.map((e) => e));
+  return (
+    <>
+      <Menu />
+      <div className="row container">
+        <div
+          className="col-4"
+          style={{ display: "flex", flexDirection: "column" }}
+        >
+          <h4 style={{ float: "right", marginLeft: 100 }}>تصفية حسب الفئات</h4>
+          <ul>
+            {photos.map((e) => (
+              <Checkbox
+                pictures={e}
+                categories={categories}
+                handleFilters={(filters) => handleFilters(filters, "category")}
+              />
+            ))}
+            <img />
+          </ul>
+        </div>
+
+        <div style={{ float: "right", marginTop: 80 }} className="col-8">
+          <h2 className="mb-4">الباعة</h2>
+          <div className="row">
+            {filteredResults.map((seller, i) => (
+              <div key={i} className="col-4 mb-3">
+                <div className="card ">
+                  <div className="card-header card-header-1 ">
+                    {seller.name}
+                  </div>
+                  <Link to={`/shop/seller/${seller._id}`}>تحقق من البائع</Link>
+                </div>
+              </div>
+            ))}
+          </div>
+          <hr />
+          {loadMoreButton()}
+        </div>
+      </div>
+      {/* <Footer position /> */}
+    </>
+  );
 };
 
 export default Shop;
